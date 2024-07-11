817e7d584a9e56557f33f9a6837c1efb7075cd76
nl-trantrungdong <trandong2805@gmail.com>
2024-07-11 17:58:55 +0700

    fist commit

---
+++ src/main/java/com/gentech/proxy/GenTechProxyApplication.java
@@ -0,0 +1,13 @@
+package com.gentech.proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenTechProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenTechProxyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gentech/proxy/gentechproxy/GenTechProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gentech.proxy.gentechproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenTechProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
